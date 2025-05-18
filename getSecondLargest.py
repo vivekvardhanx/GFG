fe56892161ec22ed81728eed95951b07578dc1a6
VIVEK <142317278+vivekvardhanx@users.noreply.github.com>
2025-05-18 22:06:40 +0530

    Update getSecondLargest.py

--- getSecondLargest.py
+++ getSecondLargest.py
@@ -1,3 +1,8 @@
+'''
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+'''
 def getSecondlargest(self,arr)
 n = len(arr)
 if n<2:
