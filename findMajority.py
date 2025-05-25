6668f351061e06a285940d2c9d3a15c8b5d17d1a
VIVEK <142317278+vivekvardhanx@users.noreply.github.com>
2025-05-25 23:59:48 +0530

    Update findMajority.py

--- findMajority.py
+++ findMajority.py
@@ -2,4 +2,5 @@
 You are given an array of integer arr[] where each number represents a vote to a candidate. 
 Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
 '''
-def findMajority(arr)
+def findMajority(arr):
+  n = len(arr)
